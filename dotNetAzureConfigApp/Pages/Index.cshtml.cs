9953ed25383fea76be7d62e8e0d3f8cb0d334d2e
Shashikant-15 <shashikant.tanti@nashtechglobal.com>
2023-11-01 14:01:24 +0530

    Add project files.

---
+++ dotNetAzureConfigApp/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+
+namespace dotNetAzureConfigApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        IConfiguration _configuration;
+        public Settings Settings { get; }
+
+        public IndexModel(ILogger<IndexModel> logger,IConfiguration configuration, IOptionsSnapshot<Settings> options)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            string configvalue = _configuration["config1"];
+            Settings = options.Value;
+
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
